6b34e82c7bb2e068108f9c82e2974feaadded671
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2025-03-02 23:47:54 +0000

    removed crosshair & applied hover overlay

--- Stirred from Stone/Assets/ObjectDetector.cs
+++ Stirred from Stone/Assets/ObjectDetector.cs
@@ -9,15 +9,15 @@ public class ObjectDetector : MonoBehaviour
     public LayerMask layersToDetect;
 
     private GameObject detectedObject;
-    public GameObject DetectedObject
-    {
-        get { return detectedObject; }
-    }
+    public GameObject DetectedObject => detectedObject; 
+
+    public Color hoverColor; // ✅ Semi-transparent white overlay
+
+    
     public CrosshairText crosshairText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -27,15 +27,56 @@ public class ObjectDetector : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, detectionDistance, layersToDetect))
         {
-            detectedObject = hit.collider.gameObject;
-            crosshairText.SetText("Press 'E' to interact with " + detectedObject.name);
-            crosshair.color = Color.green;
+            GameObject hitObject = hit.collider.gameObject;
+            
+            if (detectedObject != hitObject)
+            {
+                // changing objects
+                if (detectedObject != null)
+                {
+                    RemoveOverlay(detectedObject);
+                }
+                // detecting new object
+                detectedObject = hitObject;
+                ApplyOverlay(detectedObject);
+
+                crosshairText.SetText("Press 'E' to interact with " + detectedObject.name);
+                crosshair.color = Color.green;
+
+            }
+            
         }
         else
         {
+            if (detectedObject != null)
+            {
+                 RemoveOverlay(detectedObject);
+            }
             detectedObject = null;
             crosshair.color = Color.white;
             crosshairText.SetText("");
+        }
+    }
+
+     void ApplyOverlay(GameObject obj)
+    {
+        
+        MeshRenderer renderer = obj.GetComponent<MeshRenderer>();
+        if (renderer && renderer.sharedMaterial.HasProperty("_EmissionColor"))
+        {
+            renderer.material.SetColor("_EmissionColor", hoverColor);
+            // Ensure the material is using the emission effect by enabling it
+            renderer.material.EnableKeyword("_EMISSION");
+        }
+    }
+
+    void RemoveOverlay(GameObject obj)
+    {
+        MeshRenderer renderer = obj.GetComponent<MeshRenderer>();
+        if (renderer != null)
+        {
+            // Reset the emission color to black (or transparent effect)
+            renderer.material.SetColor("_EmissionColor", Color.black);
         }
     }
 }

e1084de02769cd8dc9b0f67dca96105146d92047
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2025-03-05 10:19:51 +0000

    saving

--- Stirred from Stone/Assets/CoffinInteractable.cs
+++ Stirred from Stone/Assets/CoffinInteractable.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using System;
+using System.Collections;
 
-public class CoffinInteractable : MonoBehaviour
+[RequireComponent(typeof(AudioSource))]
+public class CoffinInteractable : MonoBehaviour, IInteractable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    public string Name => "Coffin";
+    public MovementAnimator movementAnimator;
+    private bool isOpen = false;
+    public AudioSource audioSource;
+    public AudioClip coffinOpeningSound;
+    
+    public void Interact()
     {
-        
+        Debug.Log("Interacting with Coffin.");
+        if (!isOpen)
+        {
+            OpenCoffin();
+            
+        }
+    }
+    public void OpenCoffin()
+    {
+        isOpen = true;
+        if (audioSource && coffinOpeningSound != null)
+        {
+            audioSource.PlayOneShot(coffinOpeningSound);
+        }
+        if (movementAnimator)
+        {
+            movementAnimator.StartMovement(OnRevealComplete);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnRevealComplete()
     {
-        
+        DialogueManager.Instance.AddActionDialogue("Oh great, it's a skeleton");
     }
 }
